dd02c26f963a264b6d77a7b3b2678c3eff3f231f
Ines Cadete <inesmcadete@hotmail.com>
2026-06-24 13:08:19 +0100

    Fixed issues in article model

--- models/articles.model.js
+++ models/articles.model.js
@@ -91,11 +91,12 @@ exports.fetchArticleById = (article_id) => {
     articles.topic,
     articles.created_at,
     articles.votes,
-    articles.article_img_url
-     CAST(COUNT(comments.comment_id) AS INT) AS comment_count
+    articles.article_img_url,
+    CAST(COUNT(comments.comment_id) AS INT) AS comment_count
     FROM articles
-     LEFT JOIN comments ON articles.article_id = comments.article_id
-    WHERE articles.article_id = $1`,
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id`,
       [article_id],
     )
     .then(({ rows }) => {
